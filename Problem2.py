bc0ee4623c41a2fa29fb1c1287b841eb2fc12a61
Angiemerkaj <enxhimerkaj310@gmail.com>
2023-10-29 19:18:07 -0500

    First Commit

---
+++ Problem2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env
+
+#Enxhi Merkaj 10/29/2023
+
+numbers = [12, 10, 32, 3, 66, 17, 42, 99, 20]
+
+#write a loop that prints all the numbers
+for num in numbers:
+    print(num)
+
+#write a loop that prints all the numbers and their square root
+for num in numbers:
+    square = num * num
+    print(f"{num} and square root is {square} ")
